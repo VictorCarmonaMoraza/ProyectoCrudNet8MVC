fc1cef2683d70e90e26fe9a5904ee9954125d5be
Victo <victorcarmona9035@hotmail.com>
2024-05-17 20:44:28 +0100

    Arvivo de Contexto

---
+++ CrudBet8MVC/Datos/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudBet8MVC.Datos
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Agregar los modelos aqui(cada modelo corresponde a una tabla en la BD)
+
+        
+    }
+}
