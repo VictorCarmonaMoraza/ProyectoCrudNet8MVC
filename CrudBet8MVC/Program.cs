2b812d1cb265a7600f7d56ef3b6518fc4e7a5369
Victo <victorcarmona9035@hotmail.com>
2024-05-18 08:05:07 +0100

    Configuracion de conexión

--- CrudBet8MVC/Program.cs
+++ CrudBet8MVC/Program.cs
@@ -1,11 +1,18 @@
+using CrudBet8MVC.Datos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Configuramos la conexion a sql ser local db MSSQLLOCAL
+builder.Services.AddDbContext<ApplicationDbContext>(opciones=>
+opciones.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSql")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
